b867d0f80f0253f99df4e7aa6b8e8f9a41af9ff2
nutchayatonkunya8-boop <nutchayatonkunya8@gmail.com>
2026-09-16 15:51:08 +0700

    Update Examination_1.py

--- Examination_1.py
+++ Examination_1.py
@@ -6,4 +6,7 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-total_minutes = int(input())
+total_minutes = int(input("90นาที"))
+int(input)("จำนวนชั่วโมง:")
+print("แสดงจำนวนชั่วโมง:")
+print("แสดงนาที่ที่เหลือ:")
